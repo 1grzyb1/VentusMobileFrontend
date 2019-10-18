a4379db45dc2c71fce71130c1f0d51c445170ffc
grzybek <grzybol.k@gmail.com>
2019-10-18 16:57:27 +0200

    Login form

--- ventus/lib/Second.dart
+++ ventus/lib/Second.dart
@@ -1,17 +1,193 @@
 import 'package:flutter/material.dart';
 import 'package:ventus/main.dart';
 
-class Second extends StatelessWidget {
-// This widget is the root of your application.
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  String dropdownValue = 'Mężczyzna';
+  bool switchOn = false;
+  bool switchOn1 = false;
+  bool switchOn2 = false;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        routes: <String, WidgetBuilder>{
-          '/a': (BuildContext context) => MyApp(),
-        });
+    void _onSwitchChanged(bool variable) {
+      setState(() {
+        switchOn = variable;
+      });
+    }
+    void _onSwitchChanged1(bool variable) {
+      setState(() {
+        switchOn1 = variable;
+      });
+    }
+    void _onSwitchChanged2(bool variable) {
+      setState(() {
+        switchOn2 = variable;
+      });
+    }
+
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
+            child: Container(
+              height: 50,
+              width: 330,
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 5.0),
+                    ),
+                    labelText: 'Imię'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            child: Container(
+              height: 50,
+              width: 330,
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 5.0),
+                    ),
+                    labelText: 'Miejsce zamieszkania'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            child: Container(
+              height: 50,
+              width: 330,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1.1, color: Colors.grey[600]),
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: DropdownButton<String>(
+                  underline: SizedBox(),
+                  value: dropdownValue,
+                  icon: Padding(
+                    padding: const EdgeInsets.fromLTRB(200, 0, 0, 0),
+                    child: Icon(Icons.keyboard_arrow_down),
+                  ),
+                  iconSize: 24,
+                  style: TextStyle(fontSize: 15, color: Colors.grey[600]),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['Mężczyzna', 'Kobieta']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 30, 10, 0),
+            child: Table(
+              columnWidths: {1: FractionColumnWidth(.2)},
+              children: [
+                TableRow(children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: Text("Wyświetlaj moje zdjęcie z Facebooka",
+                        style: TextStyle(color: Color(0xff44647D),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Switch(
+                    onChanged: _onSwitchChanged,
+                    value: switchOn,
+                    activeColor: Colors.white,
+                    activeTrackColor: Color(0xff44647D),
+                    inactiveThumbColor: Colors.white,
+                    inactiveTrackColor: Colors.grey,
+                  ),
+                ]),
+                TableRow(children: [
+                  SizedBox(height: 20),
+                  SizedBox(height: 0),
+                ]),
+                TableRow(children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: Text("Pokazuj tylko osoby w pobliżu",
+                        style: TextStyle(color: Color(0xff44647D),
+                        fontWeight: FontWeight.bold)),
+                  ),
+                  Switch(
+                    onChanged: _onSwitchChanged1,
+                    value: switchOn1,
+                    activeColor: Colors.white,
+                    activeTrackColor: Color(0xff44647D),
+                    inactiveThumbColor: Colors.white,
+                    inactiveTrackColor: Colors.grey,
+                  ),
+                ]),
+                TableRow(children: [
+                  SizedBox(height: 20),
+                  SizedBox(height: 0),
+                ]),
+                TableRow(children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: Text("Mam ukończone 13 lat i akceptuje warunki użytkowania",
+                        style: TextStyle(color: Color(0xff44647D),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Switch(
+                    onChanged: _onSwitchChanged2,
+                    value: switchOn2,
+                    activeColor: Colors.white,
+                    activeTrackColor: Color(0xff44647D),
+                    inactiveThumbColor: Colors.white,
+                    inactiveTrackColor: Colors.grey,
+                  ),
+                ]),
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
+          FlatButton(
+            child: _animatedButtonUI,
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute<Null>(builder: (BuildContext context) {
+                    return new SecondScreen();
+                  }));
+            },
+          )
+        ],
+      )),
+    );
   }
 }
+
+Widget get _animatedButtonUI => Container(
+  height: 50,
+  width: 340,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(100.0),
+    color: Color(0xff44647D),
+  ),
+  child: Center(
+    child: Icon(Icons.arrow_forward, color: Colors.white,)
+  ),
+);
--- ventus/lib/main.dart
+++ ventus/lib/main.dart
@@ -6,26 +6,19 @@ import 'Second.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: "SetState management",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: FirstScreen(),
     );
   }
@@ -40,7 +33,7 @@ class FirstScreen extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(0, 200, 0, 0),
-            child: Image.asset('assets/logo.png'),
+            child: Image.asset('assets/logo07.png'),
           ),
           SizedBox(height: 200),
           FlatButton(
@@ -56,8 +49,6 @@ class FirstScreen extends StatelessWidget {
       )),
     );
   }
-
-  void _onTapDown(TapDownDetails details) {}
 }
 
 Widget get _animatedButtonUI => Container(
@@ -65,7 +56,7 @@ Widget get _animatedButtonUI => Container(
       width: 270,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100.0),
-        color: Colors.lightBlue[600],
+        color: Color(0xff44647D),
       ),
       child: Center(
         child: Text(
@@ -79,45 +70,3 @@ Widget get _animatedButtonUI => Container(
       ),
     );
 
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-            child: Container(
-              height: 50,
-              width: 370,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 5.0),
-                    ),
-                    labelText: 'Imię'
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            child: Container(
-              height: 50,
-              width: 370,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 5.0),
-                    ),
-                    labelText: 'Miejsce zamieszkania'
-                ),
-              ),
-            ),
-          ),
-        ],
-      )),
-    );
-  }
-}
